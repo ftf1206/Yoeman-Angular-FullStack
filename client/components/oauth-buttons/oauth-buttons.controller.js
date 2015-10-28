ccdd9f2ab541227e9be590127acdf235e2eb660e
t.fujimoto <fujimoto@and-s.com>
2015-10-28 11:05:01 +0900

    initial commit

---
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('alloalloApp')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
